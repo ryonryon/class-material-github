22c50e9e121ee57e0af607361c3a683583746dad
shoishizaki <shoishizaki1997@gmail.com>
2019-06-07 16:26:46 +0900

    Rewrite function

--- questions/question3/question3.py
+++ questions/question3/question3.py
@@ -1,19 +1,19 @@
 
 def add(x, y):
     # change None to add x and y
-    return None
+    return "change None to add" + x + "and" + y
 
 
 def substract(x, y):
     # change None to substract x and y
-    return None
+    return "change None to substract" + x + "and" y
 
 
 def multiple(x, y):
     # change None to multiple x and y
-    return None
+    return "change None to multiple" + x + "and" + y
 
 
 def divide(x, y):
     # change None to divide x and y
-    return None
+    return "change None to divide" + x  + "and" + y
